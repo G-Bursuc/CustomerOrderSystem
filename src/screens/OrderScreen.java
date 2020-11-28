a58df1cd2512d4b833abc6af46de6d3dc5d3ff3b
Iuliana Attl <C18379476@mytudublin.ie>
2020-11-28 17:20:49 +0000

    Display Total Cost- not finished

--- src/screens/OrderScreen.java
+++ src/screens/OrderScreen.java
@@ -1,9 +1,14 @@
 package screens;
 
 import objects.Order;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class OrderScreen  extends JFrame{
@@ -13,7 +18,26 @@ public class OrderScreen  extends JFrame{
 		list = orderList;
 		JPanel panel = new JPanel();
 		
+		//button to see total for the order
+		JButton seeTotal = new JButton("total");
+		//total variable
+		double totalDue = 0;
+		//in code when adding a product
+		//totalDue = totalDue + product.getPrice();
 		
+		//if totalDue is in a loop do this and change totalDue in info box to displayTotal
+		//double displayTotal = totalDue;
+		
+		
+		
+		
+		
+		//action listener for total button
+		seeTotal.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Total : " + totalDue, "InfoBox", JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
 		
 		add(panel);
 		setTitle("Order Screen");
