3fa55e043a98e94642e3e8b887367c054bf320a9
Tami Adeduntan <tami.adeduntan@gmail.com>
2020-11-28 17:19:29 +0000

    First push

--- src/screens/ProductScreen.java
+++ src/screens/ProductScreen.java
@@ -2,6 +2,7 @@ package screens;
 
 import objects.Product;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -13,8 +14,30 @@ public class ProductScreen  extends JFrame{
 		list = productList;
 		JPanel panel = new JPanel();
 		
+		String name;
+		String brand;
+		int stock;
+		double price;
+		int idProduct;
 		
-		
+		Scanner scan = new Scanner(System.in);
+
+		Product aProduct;
+
+		System.out.print("Enter product name: ");
+		name = scan.nextLine();
+		System.out.print("Enter brand: ");
+		brand = scan.nextLine();
+		System.out.print("Enter stock: ");
+		stock = scan.nextInt();
+		System.out.println("Enter price amount: ");
+		price = scan.nextInt();
+		System.out.print("Enter product ID: ");
+		idProduct = scan.nextInt();
+
+		aProduct = new Product(name, brand, stock, price, idProduct);
+		list.add(aProduct);
+				
 		add(panel);
 		setTitle("Product Screen");
 		setSize(580, 600);
