76447c846156791b35f0cb71fbdd41888ca28a26
jchukwuezi <jchukwuezi@gmail.com>
2020-11-28 17:54:30 +0000

    Created customer class

--- src/objects/Customer.java
+++ src/objects/Customer.java
@@ -1,9 +1,49 @@
 package objects;
 
 public class Customer {
+	private String customerID;
 	private String customerName;
+	private String customerPhoneNumber;
+	private String customerAddress;
 	
-	public Customer() {
-		
+	public Customer(String id, String name, String phNumber, String address) {
+			this.customerID = id;
+			this.customerName = name;
+			this.customerPhoneNumber = phNumber;
+			this.customerAddress = address;
 	}
+
+	public String getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(String customerID) {
+		this.customerID = customerID;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerPhoneNumber() {
+		return customerPhoneNumber;
+	}
+
+	public void setCustomerPhoneNumber(String customerPhoneNumber) {
+		this.customerPhoneNumber = customerPhoneNumber;
+	}
+
+	public String getCustomerAddress() {
+		return customerAddress;
+	}
+
+	public void setCustomerAddress(String customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+	
+	
 }
