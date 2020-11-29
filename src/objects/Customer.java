b4b9bc4983dc1cd9f5122840722b7ca74155db34
G-Bursuc <c18399946@mytudublin.ie>
2020-11-29 22:02:47 +0000

    User can now update a customer record

--- src/objects/Customer.java
+++ src/objects/Customer.java
@@ -2,7 +2,31 @@ package objects;
 
 public class Customer {
 
-	public Customer() {
-		
+	private int id;
+	private String name;
+	private String phone;
+	private String address;
+
+	public Customer(int id, String name, String phone, String address) {
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.address = address;
+	}
+	
+	public int getCustomerId() {
+		return id;
+	}
+	
+	public String getCustomerName() {
+		return this.name;
+	}
+	
+	public String getCustomerPhoneNumber() {
+		return this.phone;
+	}
+	
+	public String getCustomerAddress() {
+		return this.address;
 	}
 }
--- src/screens/CustomerScreen.java
+++ src/screens/CustomerScreen.java
@@ -1,29 +1,48 @@
 package screens;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import objects.Customer;
 
 public class CustomerScreen extends JFrame{
 	ArrayList<Customer> list = null;
-	
+
 	public CustomerScreen(ArrayList<Customer> customerList) {
 		list = customerList;
 		JPanel panel = new JPanel();
-		
-		
-		
+		JButton updateCustButton = new JButton("Update A Customer Record");
+		JButton exitButton = new JButton("Exit");
+
+		// button that brings user to update a customer record
+		updateCustButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// check if customers exist in the arraylist
+				if (!list.isEmpty())
+					new UpdateCustomerScreen(list);
+				else // if arraylist is empty then show a popup warning
+					JOptionPane.showMessageDialog(null, "Access Denied, No Customers Exist in the System", "Alert", JOptionPane.WARNING_MESSAGE);
+			}});
+
+		// button to exit the window
+		exitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+
+		panel.add(updateCustButton);
+		panel.add(exitButton);
 		add(panel);
 		setTitle("Customer Screen");
 		setSize(580, 600);
 		setVisible(true);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-	}
-	
-	public void updateCustomer() {
-		String name;
 	}
 }
