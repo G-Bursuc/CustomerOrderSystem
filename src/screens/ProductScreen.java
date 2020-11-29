00b0a0e37128026df16bac6b79eca2c09196ceba
Tami Adeduntan <tami.adeduntan@gmail.com>
2020-11-29 15:08:28 +0000

    Product Mostly Complete - main method issue unresolved

--- src/screens/ProductScreen.java
+++ src/screens/ProductScreen.java
@@ -2,6 +2,7 @@ package screens;
 
 import objects.Product;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -16,51 +17,52 @@ import javax.swing.JTextField;
 import net.miginfocom.swing.MigLayout;
 
 public class ProductScreen  extends JFrame{
+	//create list
 	ArrayList<Product> list = null;
 	
 	public ProductScreen(ArrayList<Product> productList) {
 		list = productList;
 		JPanel panel = new JPanel();
-		JLabel label = new JLabel("Enter product name: ");
-		JLabel label2 = new JLabel("Enter brand: ");
-		JLabel label3 = new JLabel("Enter stock: ");
-		JLabel label4 = new JLabel("Enter price amount: ");
-		JLabel label5 = new JLabel("Enter product ID: ");
 		
+		//label and textfields for user input
+		JLabel label = new JLabel("Enter product name: ");
 		JTextField name = new JTextField(50);
+		JLabel label2 = new JLabel("Enter brand: ");
 		JTextField brand = new JTextField(50);
+		JLabel label3 = new JLabel("Enter stock: ");
 		JTextField stock = new JTextField(50);
+		JLabel label4 = new JLabel("Enter price amount: ");
 		JTextField price = new JTextField(50);
+		JLabel label5 = new JLabel("Enter product ID: ");		
 		JTextField idProduct = new JTextField(50);
 		
+		//add button
 		JButton addButton = new JButton("Add Product");
-		JButton cancelButton = new JButton("Cancel");
 		
+		//reads data from textfields and creates a product object using the data
 		addButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Product aProduct = new Product(name.getText(), brand.getText(), Integer.parseInt(stock.getText()), Double.parseDouble(price.getText()), Integer.parseInt(idProduct.getText()));
+				list.add(aProduct);
 			}			
 		});
 		
-		cancelButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}			
-		});
-		
-		
-			
-		
-		panel.setLayout(new MigLayout ("", "275!"));
+		//add elements to the panel and configure miglayout
 		panel.add(label);
+		panel.add(name);
 		panel.add(label2);
+		panel.add(brand);
 		panel.add(label3);
+		panel.add(stock);
 		panel.add(label4);
+		panel.add(price);
 		panel.add(label5);
-		panel.add(addButton);
-		panel.add(cancelButton);				
+		panel.add(idProduct);
+		panel.add(addButton);				
 		add(panel);
 		
+		//set frame properties
+		panel.setLayout(new MigLayout ("center center, wrap, gapy 20"));
 		setTitle("Product Screen");
 		setSize(580, 600);
 		setVisible(true);
@@ -68,8 +70,8 @@ public class ProductScreen  extends JFrame{
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 	}
 	
-//	public static void main(String[] args) {
-//		
-//		
-//	}
+	public static void main(String[] args) {
+		ProductScreen productScreen = new ProductScreen(list);
+		
+	}
 }
